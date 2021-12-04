1987088a778ac09957b2d7719f9c383383114c4c
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-12-04 14:08:35 +0200

    second world before set tile

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,24 @@ const startScreenElement = document.querySelector(".start-screen")
 
 const gameBoard = document.querySelector("#game-board");
 
-const toolboxElemnt = document.querySelector(".tool-box");
+const toolboxElement = document.querySelector(".tool-box");
+const toolsElements = toolboxElement.querySelectorAll("div")
+
+// TODO: how to target nth child element in js
+// const secondToolElement = document.querySelector(".tool-box:nth-child(2)");
+
+// const firstToolElement = document.querySelector(".axe")
+// const secondToolElement = document.querySelector(".pickaxe")
+// const thirdToolElement = document.querySelector(".shovel")
+
 
 const startButton = document.querySelector(".start-button");
 
-const inventoryElement = document.querySelector(".inventory")
+const inventoryElement = document.querySelector(".inventory");
+
+const nextButton = document.querySelector(".next-button");
+
+const gameBoardNew = document.querySelector("#game-bord-new");
 
 const gameBoardMatrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -86,7 +99,6 @@ gameBoardMatrix.forEach((row, yIndex) => {
 
 //states if the player picked a tool a tile or none
 let state;
-
 let toolState = "";
 let tileState = "";
 
@@ -96,11 +108,13 @@ let tileState = "";
 const gameStart = () => {
     startScreenElement.style.display = "none";
     gameBoard.style.display = "grid";
+    nextButton.style.display = "block"
 }
 
 const getTool = (event) => {
-    state = "tool"
+    state = "tool";
     toolState = event.target.dataset.tool;
+    console.log(toolState);
 }
 
 
@@ -149,12 +163,21 @@ const hitTile = (event) => {
     }
 }
 
-
+// TODO: change if its posible to change data set tilenum to tile and to use just the original func 
 const getTile = (event) => {
+    console.log("click");
     if (inventoryElement.dataset.tile) {
         state = "tile"
         tileState = inventoryElement.dataset.tile;
+        console.log(state, tileState);
+    } else {
+        if (inventoryElement.dataset.tilenum) {
+            state = "tile"
+            tileState = inventoryElement.dataset.tilenum;
+            console.log(state, tileState);
+        }
     }   
+
 }
 
 const setTile = (event) => {
@@ -165,14 +188,93 @@ const setTile = (event) => {
     }
 }
 
+const nextWorld = (event) => {
+    gameBoard.style.display = "none";
+    gameBoardNew.style.display = "grid";
+    toolsElements[0].dataset.tool = "pink";
+    toolsElements[1].dataset.tool = "green";
+    toolsElements[2].dataset.tool = "blue";
+    state = "";
+    toolState = "";
+    tileState = "";
+}
 
-
-toolboxElemnt.addEventListener("click", (e)=>(getTool(e)))
+toolboxElement.addEventListener("click", (e)=>(getTool(e)))
 
 gameBoard.addEventListener("click", (e)=>(clickOnTheBord(e)))
 
 startButton.addEventListener("click", gameStart)
 
 inventoryElement.addEventListener("click", (e)=>(getTile(e)))
+
+
+nextButton.addEventListener("click", (e)=> (nextWorld(e)))
+
+
+
+// TODO: can i use a var both in js and css file? (like the num of colum and rows)
+
+
+const creatRandomMatrix = () => {
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+        arr.push (Math.floor(Math.random * 7));
+        arr[i] = [];
+        for (let j = 0; j < 10; j++) {
+            arr[i][j] = Math.floor(Math.random() * 7);
+            
+        }
+    }
+    return arr;
+}
+
+const newMatrix = creatRandomMatrix();
+
+const creatNewBord = (dataArr, rootElement) => {
+    console.log("test");
+    dataArr.forEach ((row, rowIndex)=>{
+        row.forEach((column, columnIndex) => {
+            let newElement = document.createElement("div");
+            newElement.dataset.tilenum = dataArr[rowIndex][columnIndex];
+            rootElement.appendChild(newElement)
+        })
+    })
+    
+}
+
+//chack if the tool that was picked and stored in toolState mach the tile that was click on the bord - returns true/false
+const canHit = () => {
+    let result;
+    switch (toolState) {
+        case "pink":  
+           result =  (tileState == "1" || tileState =="2")? true : false;
+           break;
+        case "green":
+            result =  (tileState == "3" || tileState =="4")? true : false;
+            break;
+        case "blue":
+            result =  (tileState == "5" || tileState =="6")? true : false;
+            break
+        default:
+            result = false;
+    }
+    console.log(result);
+    return result;
+    
+}
+
+// TODO: how can i desructur event.target.dataset.tilenum
+const clickNewBord = (event) => {
+    tileState = event.target.dataset.tilenum;
+    if (canHit()) {
+        inventoryElement.dataset.tilenum = tileState;
+        console.log(inventoryElement.dataset.tilenum);
+        delete event.target.dataset.tilenum;
+    }
+}
+
+creatNewBord (newMatrix, gameBoardNew);
+
+gameBoardNew.addEventListener("click", (e) => (clickNewBord(e)));
 
 
